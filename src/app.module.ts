1b11e51d1a8f61d8c91fbae8142c2c88dfcb6982
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-09-26 20:34:09 +0100

    minor fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,18 +10,16 @@ import { CacheModule } from '@nestjs/cache-manager';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
 import { LoggingInterceptor } from './app.interceptor';
-import { MulterModule } from '@nestjs/platform-express'; 
+import { MulterModule } from '@nestjs/platform-express';
 import { FileModule } from './file/file.module';
 import { HealthModule } from './health/health.module';
-
-
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     CacheModule.register(),
     MulterModule.register({
-      dest: './uploads',
+      dest: '../uploads',
     }),
     ThrottlerModule.forRoot([
       {
