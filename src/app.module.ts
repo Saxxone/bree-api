1f1dd3b74ff10ea002e58f516d5bb936ba4c5f05
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-10-02 12:35:29 +0100

    feat(notification): setup notifications

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { MulterModule } from '@nestjs/platform-express';
 import { FileModule } from './file/file.module';
 import { HealthModule } from './health/health.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
@@ -34,6 +35,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     AuthModule,
     FileModule,
     HealthModule,
+    NotificationModule,
   ],
   controllers: [AppController],
   providers: [
---
+++ src/notification/notification.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { NotificationService } from './notification.service';
+import { CreateNotificationDto } from './dto/create-notification.dto';
+import { UpdateNotificationDto } from './dto/update-notification.dto';
+
+@Controller('notification')
+export class NotificationController {
+  constructor(private readonly notificationService: NotificationService) {}
+
+  @Cron('* * 0 * * *', {
+    name: 'notifications',
+    timeZone: 'Europe/London',
+  })
+  triggerNotifications() {
+    
+  }
+
+  @Post()
+  create(@Body() createNotificationDto: CreateNotificationDto) {
+    return this.notificationService.create(createNotificationDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.notificationService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.notificationService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateNotificationDto: UpdateNotificationDto,
+  ) {
+    return this.notificationService.update(+id, updateNotificationDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.notificationService.remove(+id);
+  }
+}
