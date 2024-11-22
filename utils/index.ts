576b78b81f88863c320ad0c1c7b9a018e5003b0b
Udoekpo Stephen <42893818+Saxxone@users.noreply.github.com>
2024-11-22 11:23:14 +0000

    fix(post): return like and bookmark status at once (#4)

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,2 @@
-export const ui_base_url = process.env.UI_BASE_URL || 'http://localhost:4000';
+export const ui_base_url = process.env.UI_BASE_URL || 'https://localhost:4000';
 export const api_base_url = process.env.API_BASE_URL || 'http://localhost:3000';
